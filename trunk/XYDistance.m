cca9b109366ddb21d57518c867241e05b90bba0b
rahjix <rahjix@d84f1849-0c59-db7c-7cea-ce559b9394e1>
2011-11-26 18:15:34 +0000

    Added XY Distance patch

---
+++ trunk/XYDistance.m
@@ -0,0 +1,141 @@
+//
+//  XYDistance.m
+//  rd_qc_utils
+//
+//  Created by Rob Duarte on 11/26/11.
+//  Copyright (c) 2011 rahji.com. All rights reserved.
+//
+
+/* It's highly recommended to use CGL macros instead of changing the current context for plug-ins that perform OpenGL rendering */
+#import <OpenGL/CGLMacro.h>
+
+#import "XYDistance.h"
+
+#define	kQCPlugIn_Name				@"XY Distance"
+#define	kQCPlugIn_Description		@"Find the distance between two coordinates. Works with pixels or units.\n\nhttp://code.google.com/p/rdqcutils/"
+
+@implementation XYDistance
+
+//Here you need to declare the input / output properties as dynamic as Quartz Composer will handle their implementation
+@dynamic inputX1, inputY1, inputX2, inputY2, outputDistance;
+
++ (NSDictionary*) attributes
+{
+	//Return a dictionary of attributes describing the plug-in (QCPlugInAttributeNameKey, QCPlugInAttributeDescriptionKey...).
+	return [NSDictionary dictionaryWithObjectsAndKeys:kQCPlugIn_Name, QCPlugInAttributeNameKey, kQCPlugIn_Description, QCPlugInAttributeDescriptionKey, nil];
+}
+
++ (NSDictionary*) attributesForPropertyPortWithKey:(NSString*)key
+{
+	//Specify the optional attributes for property based ports (QCPortAttributeNameKey, QCPortAttributeDefaultValueKey...).
+    
+    if([key isEqualToString:@"inputX1"])
+        return [NSDictionary dictionaryWithObjectsAndKeys:
+                @"X1", QCPortAttributeNameKey,
+                @"0", QCPortAttributeDefaultValueKey,
+                nil];
+    if([key isEqualToString:@"inputY1"])
+        return [NSDictionary dictionaryWithObjectsAndKeys:
+                @"Y1", QCPortAttributeNameKey,
+                @"0", QCPortAttributeDefaultValueKey,
+                nil];
+    if([key isEqualToString:@"inputX2"])
+        return [NSDictionary dictionaryWithObjectsAndKeys:
+                @"X2", QCPortAttributeNameKey,
+                @"0", QCPortAttributeDefaultValueKey,
+                nil];
+    if([key isEqualToString:@"inputY2"])
+        return [NSDictionary dictionaryWithObjectsAndKeys:
+                @"Y2", QCPortAttributeNameKey,
+                @"0", QCPortAttributeDefaultValueKey,
+                nil];
+    
+    if([key isEqualToString:@"outputDistance"])
+        return [NSDictionary dictionaryWithObjectsAndKeys:
+                @"Distance", QCPortAttributeNameKey,
+                nil];
+    
+	return nil;
+}
+
++ (NSArray*) sortedPropertyPortKeys
+{
+    return [NSArray arrayWithObjects:@"inputX1",@"inputY1",@"inputX2",@"inputY2",@"outputDistance",nil];
+}
+
++ (QCPlugInExecutionMode) executionMode
+{
+	//Return the execution mode of the plug-in: kQCPlugInExecutionModeProvider, kQCPlugInExecutionModeProcessor, or kQCPlugInExecutionModeConsumer.
+	return kQCPlugInExecutionModeProcessor;
+}
+
++ (QCPlugInTimeMode) timeMode
+{
+	//Return the time dependency mode of the plug-in: kQCPlugInTimeModeNone, kQCPlugInTimeModeIdle or kQCPlugInTimeModeTimeBase.
+	return kQCPlugInTimeModeNone;
+}
+
+- (id) init
+{
+	if(self = [super init]) {
+		//Allocate any permanent resource required by the plug-in.
+	}
+	
+	return self;
+}
+
+- (void) finalize
+{
+	//Release any non garbage collected resources created in -init.
+	[super finalize];
+}
+
+- (void) dealloc
+{
+	//Release any resources created in -init.	
+	[super dealloc];
+}
+
+@end
+
+@implementation XYDistance (Execution)
+
+- (BOOL) startExecution:(id<QCPlugInContext>)context
+{
+	//Called by Quartz Composer when rendering of the composition starts: perform any required setup for the plug-in.
+	//Return NO in case of fatal failure (this will prevent rendering of the composition to start).
+	return YES;
+}
+
+- (void) enableExecution:(id<QCPlugInContext>)context
+{
+	//Called by Quartz Composer when the plug-in instance starts being used by Quartz Composer.
+}
+
+- (BOOL) execute:(id<QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary*)arguments
+{
+	/*
+     Called by Quartz Composer whenever the plug-in instance needs to execute.
+     Only read from the plug-in inputs and produce a result (by writing to the plug-in outputs or rendering to the destination OpenGL context) within that method and nowhere else.
+     Return NO in case of failure during the execution (this will prevent rendering of the current frame to complete).
+     
+     The OpenGL context for rendering can be accessed and defined for CGL macros using:
+     CGLContextObj cgl_ctx = [context CGLContextObj];
+     */
+    
+    self.outputDistance =  sqrt( pow(self.inputX2 - self.inputX1, 2) + pow(self.inputY2 - self.inputY1, 2) );   
+    
+	return YES;
+}
+
+- (void) disableExecution:(id<QCPlugInContext>)context
+{
+	//Called by Quartz Composer when the plug-in instance stops being used by Quartz Composer.
+}
+
+- (void) stopExecution:(id<QCPlugInContext>)context
+{
+	//Called by Quartz Composer when rendering of the composition stops: perform any required cleanup for the plug-in.
+}
+
+@end
